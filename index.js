bdde80aba3823b4270dc3f9a977a9038ee3efa9d
Sergey <loss2004@yandex.ru>
2016-05-28 22:05:37 +0300

    '546456'

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ var http = require("http")
 var express = require("express")
 var app = express()
 var port = process.env.PORT || 5000
-port = 5000
 
 app.use(express.static(__dirname + "/"))
 
 var server = http.createServer(app)
-server.listen(port)
+server.listen(5000)
 
 console.log("http server listening on %d", port)
 
