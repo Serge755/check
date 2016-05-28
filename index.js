1a539e50619b631010a283a37088563c8997e773
Sergey <loss2004@yandex.ru>
2016-05-28 22:11:01 +0300

    '44444'

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var WebSocketServer = require("ws").Server
 var http = require("http")
 var express = require("express")
 var app = express()
-var port = process.env.PORT || 5000
+var port = process.env.PORT
 
 app.use(express.static(__dirname + "/"))
 
 var server = http.createServer(app)
-server.listen(5000)
+server.listen(port)
 
 console.log("http server listening on %d", port)
 
